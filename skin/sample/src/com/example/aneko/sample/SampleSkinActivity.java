e36deca12a0e6a53aa0548aa2f4bbd2d0c260149
lllllT <lllllT@tamanegi.org>
2011-08-18 01:36:29 +0000

    up

---
+++ skin/sample/src/com/example/aneko/sample/SampleSkinActivity.java
@@ -0,0 +1,15 @@
+package com.example.aneko.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleSkinActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // nothing to do: just for place holder
+        finish();
+    }
+}
